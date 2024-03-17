40c1cbb54b6c8577cebfbdbcc5a60d7bd3aabee5
Douglas De Sousa Silva <162925038+douglasousa49@users.noreply.github.com>
2024-03-17 16:08:30 -0300

    Add files via upload

---
+++ Exercicio_06_PI.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+// Exercicio 6, Projeto Integrador
+// Douglas Sousa
+// Adrian Sousa
+// Alex Dutra
+
+public class Exercicio06 {
+  public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Digite a velocidade média em Km/h: ");
+        float velocidade = ler.nextFloat();
+
+        System.out.println("Digite a distancia em Km: ");
+        float distancia = ler.nextFloat();
+
+        if (velocidade < 0 || distancia < 0) {
+          System.out.println("Velocidade e distancia não podem ser valores negativos");
+        }
+          
+      else {
+
+        float tempo = distancia / velocidade;        
+
+        float resultado = tempo * 60;
+
+        System.out.printf("O tempo de viagem foi de %.0f minutos.", resultado);
+        System.out.println();
+      
+      }
+
+
+  }
+}
